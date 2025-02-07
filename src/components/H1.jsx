b056e5c88ac961ad869e2707fd6d91d9f5d2c020
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-02-07 14:39:21 +0000

    Created and styled H1 and used it to add site title to Header

---
+++ src/components/H1.jsx
@@ -0,0 +1,9 @@
+export default function H1({text}) {
+
+    return (
+        <h1 className="text-4xl font-bold">
+            {text}
+        </h1>
+    )
+
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
+import H1 from "./H1";
 import Nav from "./Nav";
 
 export default function Header() {
 
     return (
-        <div className="bg-[#3b292b] text-[#e6e6e6]
-        p-4">
-            <Nav />
+        <div className="flex flex-col items-center
+        bg-[#3b292b] text-[#e6e6e6]
+        p-4 gap-4">
+            <div>
+                <H1 text='emily vhs' />
+            </div>
+            <div>
+                <Nav />
+            </div>
         </div>
     )
 
