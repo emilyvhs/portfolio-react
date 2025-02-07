9907dd90f52ac3d7b51dc694541ecf30fc2f584f
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-02-07 14:58:52 +0000

    Created TextBoxLarge and added to HomePage; styled intro section

---
+++ src/components/TextBoxLarge.jsx
@@ -0,0 +1,8 @@
+export default function TextBoxLarge({text}) {
+
+    return (
+        <p className="text-2xl">
+            {text}
+        </p>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,16 @@
+import TextBoxLarge from "../components/TextBoxLarge";
+
 export default function HomePage() {
 
     return (
-        <div>
-            <p>
-                HomePage
-            </p>
-        </div>
+        <section className="text-center p-20
+        bg-[#6b4f4f] text-[#e6e6e6]">
+            <div>
+                <TextBoxLarge text="I'm a software developer based in Derby, UK." />
+                <TextBoxLarge text="This is what I'm working on." />
+            </div>
+        </section>      
+                    
     )
     
 }
