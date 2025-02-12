eece10f770c2548ed0b730221f16a343863cfd56
Emily Sherwood <emilyvhsherwood@gmail.com>
2025-02-12 08:42:41 +0000

    Added NavLink hover styling

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -3,7 +3,8 @@ import { Link } from "react-router-dom";
 export default function NavLinks({link, linkText}) {
 
     return (
-        <div className="underline font-tomorrow text-lg">
+        <div className="underline font-tomorrow text-lg
+        hover:bg-[#6b4f4f]">
             <Link to={link}>{linkText}</Link>
         </div>
     )
